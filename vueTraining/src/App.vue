bf1c4ab4a59236512d38acb1deae3c8fd3323171
Luiz Marangoni <98007645+lzmarangoni@users.noreply.github.com>
2025-06-12 14:55:30 -0300

    Contador simplese config da aplicação

---
+++ vueTraining/src/App.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import Counter from './components/Counter.vue';
+
+</script>
+
+<template>
+  <div>
+    <Counter/>
+  </div>
+</template>
+
---
+++ vueTraining/src/components/Counter.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>Contador</h2>
+        <p>Valor atual: {{ count }}</p>
+        <button @click="increment">Incrementar</button>
+
+        </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+const count = ref(0)
+
+function increment () {
+    count.value++
+}
+
+</script>
